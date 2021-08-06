ee8fd49d288baf77fdd38c075ce13ae84213700c
Nicolas Corbalan <outreef@hotmail.com>
2021-08-06 21:55:00 +0200

    footer

---
+++ components/Footer.js
@@ -0,0 +1,37 @@
+function Footer() {
+  return (
+    <div>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Nico project</p>
+        <p>Airbnb</p>
+        <p>React</p>
+        <p>Nextjs</p>
+        <p>Tailwind</p>
+      </div>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className="font-bold">HOST</h5>
+        <p>Help & Center</p>
+        <p>Trust & Safety</p>
+        <p>Another click</p>
+        <p>Place to live</p>
+        <p>Portfolio</p>
+      </div>
+      <div className='space-y-4 text-xs text-gray-800'>
+      <h5 className="font-bold">SUPPORT</h5>
+      </div>
+      <div></div>
+    </div>
+
+  )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Banner from '../components/Banner';
 import SmallCard from '../components/SmallCard';
 import MediumCard from '../components/MediumCard';
 import LargeCard from '../components/LargeCard';
+import Footer from '../components/Footer';
 
 export default function Home({ exploreData, cardsData }) {
   return (
@@ -40,8 +41,11 @@ export default function Home({ exploreData, cardsData }) {
         <LargeCard
           img='https://links.papareact.com/4cj'
           title='The Greatest Outdoors'
+          description='Wishlists curated by Airbnb'
+          buttonText='Get inspired'
         />
       </main>
+      <Footer />
     </div>
   )
 }
